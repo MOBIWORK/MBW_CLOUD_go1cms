067193339adc9ed46e3b7000dc547250fa1ad943
Bi100401 <phongtran100401@gmail.com>
2025-04-17 16:34:18 +0700

    udate job

--- go1_cms/api/website/jobs.py
+++ go1_cms/api/website/jobs.py
@@ -25,19 +25,29 @@ from frappe.utils import now, add_to_date
 
 @frappe.whitelist(allow_guest=True)
 def get_filter_job():
-    job_type = frappe.db.get_all('Employment Type', fields=[
-                                 'employee_type_name as label', 'employee_type_name as value'], order_by='creation')
+    # job_type = frappe.db.get_all('ATS_JobOpening', fields=[
+    #                              'employee_type_name as label', 'employee_type_name as value'], order_by='creation')
+    job_type = []
+    meta = frappe.get_meta('ATS_JobOpening')
+    field = meta.get_field('jo_work_form')
+    if field.options:
+        options = field.options.split('\n')
+        job_type = [{
+            "label" : item,
+            "value" : item
+        } for item in options]
+        
+    
     job_location = frappe.db.get_all(
-        'Branch', fields=['branch as label', 'branch as value'], order_by='creation asc')
+        'ATS_Location', fields=['location_name as label', 'name as value'], order_by='creation asc')
+    
     job_department = frappe.db.get_all(
-        'Department', fields=['department_name as label', 'name as value'], order_by='creation asc')
+        'ATS_Unit', fields=['unit_name as label', 'name as value'], order_by='creation asc')
+    
+    
+    job_designation = frappe.db.get_all('ATS_Position', fields=[
+        'position_name as label', 'name as value'], order_by='creation')
 
-    job_designation = frappe.db.get_all('Designation', fields=[
-        'designation_name as label', 'designation_name as value'], order_by='creation')
-    # job_designation.insert(0, {
-    #     "label": "Tất cả",
-    #     "value": "",
-    # })
     return {
         'job_type': job_type,
         'job_location': job_location,
@@ -72,10 +82,10 @@ def get_all_job(name_section, **kwargs):
     if kwargs.get('sort_by', 'desc').lower() == "asc":
         sort_by = frappe.qb.asc
 
-    JobOpening = frappe.qb.DocType('Job Opening')
+    JobOpening = frappe.qb.DocType('ATS_JobOpening')
 
     doc_section = frappe.get_doc('Page Section', name_section)
-    sort_field = doc_section.sort_field if doc_section.sort_field else 'posted_on'
+    sort_field = doc_section.sort_field if doc_section.sort_field else 'jo_application_deadline'
     limit = doc_section.no_of_records if doc_section.no_of_records else page_len
     offset = page_no*limit
 
@@ -85,13 +95,13 @@ def get_all_job(name_section, **kwargs):
         m_query = m_query.where(
             JobOpening.job_title.like('%' + text_search+'%'))
     if job_location:
-        m_query = m_query.where(JobOpening.location.isin(job_location))
+        m_query = m_query.where(JobOpening.jo_location.isin(job_location))
     if job_type:
-        m_query = m_query.where(JobOpening.employment_type.isin(job_type))
+        m_query = m_query.where(JobOpening.jo_work_form.isin(job_type))
     if job_department:
-        m_query = m_query.where(JobOpening.department.isin(job_department))
+        m_query = m_query.where(JobOpening.jo_using_unit.isin(job_department))
     if job_designation:
-        m_query = m_query.where(JobOpening.designation.isin(job_designation))
+        m_query = m_query.where(JobOpening.jo_position.isin(job_designation))
 
     if isinstance(range_salary, list):
         q = None
@@ -104,23 +114,26 @@ def get_all_job(name_section, **kwargs):
             salary_to = int(salary_to)
 
         if salary_from != None and salary_to == None:
-            q = (JobOpening.upper_range >= salary_from)
+            q = (JobOpening.jo_max_salary >= salary_from)
         elif salary_from == None and salary_to != None:
-            q = (JobOpening.lower_range <= salary_to)
+            q = (JobOpening.jo_min_salary <= salary_to)
         elif salary_from != None and salary_to != None:
-            q = ((JobOpening.lower_range <= salary_to) & (
-                JobOpening.upper_range >= salary_from))
+            q = ((JobOpening.jo_min_salary <= salary_to) & (
+                JobOpening.jo_max_salary >= salary_from))
         if q:
             m_query = m_query.where(q)
-    q_data = m_query.select(JobOpening.name, JobOpening.job_title, JobOpening.posted_on,
-                            JobOpening.location, JobOpening.employment_type, JobOpening.lower_range,
-                            JobOpening.upper_range, JobOpening.publish_salary_range, JobOpening.salary_per, JobOpening.department, JobOpening.route, JobOpening.vacancies, JobOpening.job_requisition, JobOpening.designation, JobOpening.currency
+    q_data = m_query.select(JobOpening.name, JobOpening.jo_public_title, JobOpening.jo_application_deadline,
+                            JobOpening.jo_location, JobOpening.jo_work_form, JobOpening.jo_min_salary,
+                            JobOpening.jo_max_salary, JobOpening.jo_position, JobOpening.applicants_applied, JobOpening.jo_position, JobOpening.jo_currency
                             ).offset(offset).limit(limit).orderby(JobOpening[sort_field], order=sort_by)
 
     jobs = q_data.run(as_dict=True)
 
+    
+
     for j in jobs:
-        j['pretty_posted_on'] = pretty_date(j.posted_on)
+        dt = datetime.combine(j.jo_application_deadline, datetime.min.time())
+        j['pretty_posted_on'] = pretty_date(dt)
 
     q_count = m_query.select(fn.Count('*').as_('total'))
     rs_count = q_count.run(as_dict=True)
@@ -139,9 +152,9 @@ def get_all_job(name_section, **kwargs):
 
 @frappe.whitelist(allow_guest=True)
 def get_job_detail(name):
-    if frappe.db.exists("Job Opening", name):
-        doc = frappe.db.get_value('Job Opening', name, [
-                                  'name', 'company', 'job_title', 'designation', 'status', 'posted_on', 'closes_on', 'closed_on', 'employment_type', 'department', 'location', 'publish_applications_received', 'description', 'currency', 'lower_range', 'upper_range', 'salary_per', 'publish_salary_range', 'route', 'job_requisition', 'vacancies'], as_dict=1)
+    if frappe.db.exists("ATS_JobOpening", name):
+        doc = frappe.db.get_value('ATS_JobOpening', name, [
+                                  'name', 'jo_public_title', 'jo_position', 'status', 'jo_application_deadline', 'jo_work_form', 'jo_using_unit', 'jo_location', 'jo_job_description', 'jo_currency', 'jo_min_salary', 'jo_max_salary', 'applicants_applied'], as_dict=1)
         return doc
     else:
         frappe.throw(_('Không tìm thấy công việc ứng tuyển'),
@@ -151,9 +164,9 @@ def get_job_detail(name):
 @frappe.whitelist(allow_guest=True)
 def get_job_related(name, **kwargs):
     jobs = []
-    if frappe.db.exists("Job Opening", name):
-        doc = frappe.db.get_value('Job Opening', name, [
-            'name', 'job_title', 'designation', 'status', 'employment_type', 'location'], as_dict=1)
+    if frappe.db.exists("ATS_JobOpening", name):
+        doc = frappe.db.get_value('ATS_JobOpening', name, [
+            'name', 'jo_public_title', 'jo_position', 'status', 'jo_work_form', 'jo_location'], as_dict=1)
 
         name_section = kwargs.get('name_section', None)
         if name_section:
@@ -167,34 +180,35 @@ def get_job_related(name, **kwargs):
             else:
                 limit = 4
 
-        JobOpening = frappe.qb.DocType('Job Opening')
+        JobOpening = frappe.qb.DocType('ATS_JobOpening')
         m_query = (frappe.qb.from_(JobOpening).where(
             (JobOpening.name != name)))
         q = None
 
-        if doc.location:
-            q = (q | (JobOpening.location == doc.location)) if q else (
-                JobOpening.location == doc.location)
-        if doc.designation:
-            q = (q | (JobOpening.designation == doc.designation)) if q else (
-                JobOpening.designation == doc.designation)
-        if doc.employment_type:
-            q = (q | (JobOpening.employment_type == doc.employment_type)) if q else (
-                JobOpening.employment_type == doc.employment_type)
-        if doc.job_title:
-            q = (q | (JobOpening.job_title.like('%' + doc.job_title+'%'))) if q else (
-                JobOpening.job_title.like('%' + doc.job_title+'%'))
+        if doc.jo_location:
+            q = (q | (JobOpening.jo_location == doc.jo_location)) if q else (
+                JobOpening.jo_location == doc.jo_location)
+        if doc.jo_position:
+            q = (q | (JobOpening.jo_position == doc.jo_position)) if q else (
+                JobOpening.jo_position == doc.jo_position)
+        if doc.jo_work_form:
+            q = (q | (JobOpening.jo_work_form == doc.jo_work_form)) if q else (
+                JobOpening.jo_work_form == doc.jo_work_form)
+        if doc.jo_public_title:
+            q = (q | (JobOpening.jo_public_title.like('%' + doc.jo_public_title+'%'))) if q else (
+                JobOpening.jo_public_title.like('%' + doc.jo_public_title+'%'))
 
         if q:
             m_query = m_query.where(q)
-        q_data = m_query.select(JobOpening.name, JobOpening.job_title, JobOpening.posted_on,
-                                JobOpening.location, JobOpening.employment_type, JobOpening.lower_range, JobOpening.route,
-                                JobOpening.upper_range, JobOpening.publish_salary_range, JobOpening.salary_per, JobOpening.department, JobOpening.vacancies, JobOpening.job_requisition, JobOpening.designation, JobOpening.currency
-                                ).limit(limit).orderby(JobOpening.posted_on, order=frappe.qb.desc)
+        q_data = m_query.select(JobOpening.name, JobOpening.jo_public_title, JobOpening.jo_application_deadline,
+                                JobOpening.jo_location, JobOpening.jo_work_form, JobOpening.jo_min_salary,
+                                JobOpening.jo_max_salary, JobOpening.jo_using_unit, JobOpening.applicants_applied, JobOpening.jo_position, JobOpening.jo_currency
+                                ).limit(limit).orderby(JobOpening.jo_application_deadline, order=frappe.qb.desc)
 
         jobs = q_data.run(as_dict=True)
         for j in jobs:
-            j['pretty_posted_on'] = pretty_date(j.posted_on)
+            dt = datetime.combine(j.jo_application_deadline, datetime.min.time())
+            j['pretty_posted_on'] = pretty_date(dt)
 
     return {"jobs": jobs}
 
@@ -204,7 +218,6 @@ def upload_cv(name_job, **kwargs):
     applicant_name = kwargs.get('full_name', None)
     email = kwargs.get('email', None)
     phone_number = kwargs.get('phone_number', None)
-    message = kwargs.get('message', None)
     files = frappe.request.files
     captcha_text = kwargs.get('captcha_text', None)
     form_name = kwargs.get('form_name', None)
@@ -236,13 +249,12 @@ def upload_cv(name_job, **kwargs):
     if not phone_number:
         frappe.throw('Số điện thoại không được để trống')
 
-    if frappe.db.exists("Job Opening", name_job):
-        new_doc = frappe.new_doc('Job Applicant')
-        new_doc.applicant_name = applicant_name
-        new_doc.email_id = email
-        new_doc.phone_number = phone_number
-        new_doc.cover_letter = message
-        new_doc.job_title = name_job
+    if frappe.db.exists("ATS_JobOpening", name_job):
+        new_doc = frappe.new_doc('ATS_Candidate')
+        new_doc.can_full_name = applicant_name
+        new_doc.can_email = email
+        new_doc.can_phone = phone_number
+        new_doc.job_opening_id = name_job
         new_doc.save(ignore_permissions=True)
         new_doc.reload()
 
@@ -295,26 +307,25 @@ def upload_cv(name_job, **kwargs):
         domain = get_domain()
         redirect_to = f'{domain}/app/job-applicant/{new_doc.name}'
         job_open = frappe.db.get_value(
-            'Job Opening', name_job,
-            ['job_title', 'employment_type', 'location',
-                'department', 'designation', 'lower_range', 'upper_range', 'currency', 'salary_per'],
+            'ATS_JobOpening', name_job,
+            ['jo_public_title', 'jo_work_form', 'jo_location',
+                'jo_using_unit', 'jo_position', 'jo_min_salary', 'jo_max_salary', 'jo_currency'],
             as_dict=1
         )
         args = {
             'time': new_doc.creation.strftime("%d/%m/%Y %H:%M:%S"),
-            'job_title': job_open.job_title,
-            'designation': job_open.designation,
-            'location': job_open.location,
-            'employment_type': job_open.employment_type,
-            'department': job_open.department,
-            'lower_range': job_open.lower_range,
-            'upper_range': job_open.upper_range,
-            'currency': job_open.currency,
-            'salary_per': 'Tháng' if job_open.salary_per == 'Month' else 'Năm',
+            'job_title': job_open.jo_public_title,
+            'designation': job_open.jo_position,
+            'location': job_open.jo_location,
+            'employment_type': job_open.jo_work_form,
+            'department': job_open.jo_using_unit,
+            'lower_range': job_open.jo_min_salary,
+            'upper_range': job_open.jo_max_salary,
+            'currency': job_open.jo_currency,
+            'salary_per': 'Tháng',
             'full_name': applicant_name,
             'email': email,
             'phone_number': phone_number,
-            'content': message,
             'redirect_to': redirect_to,
         }
         send_email_manage(None, 'email_apply_cv_manage', args)
