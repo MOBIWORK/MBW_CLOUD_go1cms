a065a8f25897ffa62f9c2e92c7d50f14f0ce95ff
danhhaole <danhhao.le@gmail.com>
2025-06-03 17:28:54 +0700

    haold update

--- go1_cms/go1_cms/webhook/ats.py
+++ go1_cms/go1_cms/webhook/ats.py
@@ -3,7 +3,9 @@ from go1_cms.utlis.auth import secure_webhook
 from go1_cms.go1_cms.webhook.handler import parse_webhook_data_batch
 from frappe import _
 import json
-#Sync lần đầu
+
+# Sync lần đầu
+
 
 @secure_webhook()
 def receive_webhook():
@@ -17,41 +19,42 @@ def receive_webhook():
     """
     try:
         payload = frappe.request.get_json()
-        
+
         # Kiểm tra định dạng tối thiểu
         if not payload or "doctype" not in payload or "records" not in payload:
             frappe.throw(_("Invalid payload: require 'doctype' and 'records'"))
 
         # Gọi xử lý batch
-        results = parse_webhook_data_batch(payload)
+        # Gửi webhook
+        frappe.enqueue(
+            "go1_cms.go1_cms.webhook.handler.parse_webhook_data_batch",
+            queue="short",
+            timeout=300,
+            now=True,
+            payload=payload,
+        )
+        # results = parse_webhook_data_batch(payload)
 
-        return {
-            "status": "completed",
-            "total": len(results),
-            "success_count": sum(1 for r in results if r["status"] == "success"),
-            "error_count": sum(1 for r in results if r["status"] == "error"),
-            "results": results
-        }
+        return {"status": "completed"}
 
     except Exception as e:
         frappe.log_error(frappe.get_traceback(), "Webhook Receive Error")
-        return {
-            "status": "error",
-            "message": str(e)
-        }
+        return {"status": "error", "message": str(e)}
+
 
 @secure_webhook()
 def ats_job_parse():
     if frappe.request.method != "POST":
-            frappe.throw(_("Only POST method is allowed"))
+        frappe.throw(_("Only POST method is allowed"))
     data = frappe.request.data
     if not data:
         frappe.throw(_("No data received"))
 
+
 @secure_webhook()
 def ats_candidate_parse():
     if frappe.request.method != "POST":
-            frappe.throw(_("Only POST method is allowed"))
+        frappe.throw(_("Only POST method is allowed"))
     data = frappe.request.data
     if not data:
         frappe.throw(_("No data received"))
