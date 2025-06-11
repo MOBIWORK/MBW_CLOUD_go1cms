9ea34398750eaa9e5a08c735261e91a1619d4d01
danhhaole <danhhao.le@gmail.com>
2025-06-11 08:12:16 +0700

    haold update hook

--- go1_cms/hooks.py
+++ go1_cms/hooks.py
@@ -157,6 +157,10 @@ doc_events = {
         "on_update": "go1_cms.go1_cms.webhook.handler.handle_doc_event"
         # "before_delete": "go1_cms.api.sync_data.check_sync_flags"
     },
+    "ATS_Candidate_Onboarding_Step": {
+        "on_update": "go1_cms.go1_cms.webhook.handler.handle_doc_event"
+        # "before_delete": "go1_cms.api.sync_data.check_sync_flags"
+    },
     "LMS Quiz":{
         "on_update": "go1_cms.go1_cms.webhook.handler.handle_doc_event"
         }
